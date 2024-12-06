87ea4cc39e6bba4007e3f4cc1fc125621619a7f4
Benjamin Dass <65924137+ben-dass@users.noreply.github.com>
2024-12-06 15:53:59 -0500

    completed Forms

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,7 +1,46 @@
 from django import forms
 
+from .models import Review
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(
-        label="User",
-    )
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(
+#         label="User",
+#         max_length=100,
+#         error_messages={
+#             "required": "User must not be empty",
+#             "max_length": "Please enter a shorter name",
+#         },
+#     )
+#
+#     review_text = forms.CharField(
+#         label="Feedback",
+#         widget=forms.Textarea,
+#         max_length=200,
+#     )
+#
+#     rating = forms.IntegerField(
+#         label="Rating",
+#         min_value=1,
+#         max_value=5,
+#     )
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+
+        fields = "__all__"
+
+        labels = {
+            "user_name": "User",
+            "review_text": "Review",
+            "rating": "Rating",
+        }
+
+        error_messages = {
+            "user_name": {
+                "required": "User must not be empty",
+                "max_length": "Please enter a shorter name",
+            }
+        }
