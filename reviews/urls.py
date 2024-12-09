9815eaec0d52fcf7bccd193dce94f180d3bbdd92
Benjamin Dass <65924137+ben-dass@users.noreply.github.com>
2024-12-09 12:28:43 -0500

    completed class based views

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.ReviewView.as_view(), name="reviews"),
-    path("thank-you/", views.thank_you),
+    path("thank-you/", views.ThankYouView.as_view(), name="thank-you"),
+    path("reviews", views.ReviewsListView.as_view(), name="review-list"),
+    path("reviews/<int:pk>", views.SingleReviewView.as_view(), name="review-detail"),
 ]
